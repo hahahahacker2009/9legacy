b8d8c3977b47caf612717cb246ec3d6728d164f4
Someone <hahahahacker2009@gmail.com>
2026-03-09 22:26:25 +0700

    Update build.tcl

--- .github/workflows/build.tcl
+++ .github/workflows/build.tcl
@@ -18,10 +18,7 @@ expect "term% "
 send "cd /sys/src\r"
 expect "term% "
 send "objtype=386 mk all clean cleanlibs\r"
-expect "term% "
-send "objtype=amd64 mk all clean cleanlibs\r"
-expect "term% "
-send "objtype=arm mk all clean cleanlibs\r"
+# you must have separate workflows for each arch :)
 #expect "term% "
 #send "mk release clean\r"
 
