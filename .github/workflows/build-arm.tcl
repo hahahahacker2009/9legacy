15ab5b3eed73a063af296dfe163bc15c16413ea4
Someone <hahahahacker2009@gmail.com>
2026-03-09 22:27:38 +0700

    Create build-arm.tcl

---
+++ .github/workflows/build-arm.tcl
@@ -0,0 +1,32 @@
+#!/usr/bin/expect
+
+set timeout 20
+spawn sh boot/qemu -ci
+#exp_internal 1
+
+# Boot up
+expect "Selection: "
+send "3\r"
+expect "bootfile: "
+send "2\r"
+expect "\\\[tcp\\\]: "
+sleep 1
+send "tcp\r"
+
+# Build sources
+expect "term% "
+send "cd /sys/src\r"
+expect "term% "
+send "objtype=arm mk all clean cleanlibs\r"
+# you must have separate workflows for each arch :)
+#expect "term% "
+#send "mk release clean\r"
+
+# Halt
+expect "term% "
+send "fshalt\r"
+expect "done halting"
+
+# Enter QEMU monitor
+send "\x1d" ; # Ctrl+Alt+2
+send "close\r"
